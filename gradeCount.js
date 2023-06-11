04c6cf753eb20417d33913d30c1c9ea784695640
Pablo <pablito@me.com>
2023-06-11 22:15:07 +0100

    Deleted duplicate files

--- gradeCount.js
+++
@@ -1,5 +0,0 @@
-const gradeCount = (grade, array) => {
-  return array.filter(item => item === grade).length;
-}
-
-module.exports = gradeCount;
--- names.js
+++
@@ -1,9 +0,0 @@
-const namesShuffle = (names) => {
-  return names.split(" ").reverse().join(" ")
-}
-
-const getChar = (num) => {
-  return String.fromCharCode(num)
-}
-
-module.exports = {namesShuffle, getChar};
