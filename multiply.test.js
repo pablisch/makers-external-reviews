ae7b75922c9baf1c1840efb5c9189c75ceb0514e
Pablo <pablito@me.com>
2023-06-11 18:40:21 +0100

    Renamed design.md to bandpassFilterDesign.md

---
+++ multiply.test.js
@@ -0,0 +1,34 @@
+
+const multiply = require('./multiply');
+const divide = require('./divide');
+const subtract = require('./subtract');
+
+describe('multiply function', () => { 
+  test('multiply 2 * 3 to equal 6', () => {
+    expect(multiply(2, 3)).toBe(6);
+  });
+}
+);
+
+describe('divide function', () => {
+  test('divide 6 / 3 to equal 2', () => {
+    expect(divide(6, 3)).toBe(2);
+  });
+}
+);
+
+describe('subtract function', () => {
+  test('subtract 6 - 3 to equal 3', () => {
+    expect(subtract(6, 3)).toBe(3);
+  });
+}
+);
+
+
+
+
+
+
+
+
+
